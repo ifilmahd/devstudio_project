b55e52804de18b850b37e10a0ae35b1bde77c76f
ifilmahd <65521650+ifilmahd@users.noreply.github.com>
2024-05-15 15:51:55 +0200

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,32 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const dropdownToggle = document.querySelector('[data-mdb-dropdown-init]');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
 
     dropdownToggle.addEventListener('click', function(event) {
         event.preventDefault();
-        const dropdownMenu = this.nextElementSibling;
-        const isOpen = dropdownMenu.classList.contains('show');
-
-        // Close all dropdowns before opening this one
-        closeAllDropdowns();
-        
-        // Toggle the 'show' class on the dropdown menu
-        if (!isOpen) {
-            dropdownMenu.classList.add('show');
-        }
+        dropdownMenu.classList.toggle('show');
     });
-
-    // Function to close all dropdowns
-    function closeAllDropdowns() {
-        const dropdownMenus = document.querySelectorAll('.dropdown-menu.show');
-        dropdownMenus.forEach(function(dropdownMenu) {
-            dropdownMenu.classList.remove('show');
-        });
-    }
 
     // Close dropdown when clicking outside
     document.addEventListener('click', function(event) {
         if (!event.target.closest('.dropdown')) {
-            closeAllDropdowns();
+            dropdownMenu.classList.remove('show');
         }
     });
 });
