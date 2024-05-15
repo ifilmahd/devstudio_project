a31535350d86e463624bf1a37922854539509216
ifilmahd <65521650+ifilmahd@users.noreply.github.com>
2024-05-15 16:02:31 +0200

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -5,12 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
     dropdownToggle.addEventListener('click', function(event) {
         event.preventDefault();
         dropdownMenu.classList.toggle('show');
+        dropdownMenu.classList.toggle('top'); // Toggle the 'top' class dynamically
     });
 
     // Close dropdown when clicking outside
     document.addEventListener('click', function(event) {
         if (!event.target.closest('.dropdown')) {
             dropdownMenu.classList.remove('show');
+            dropdownMenu.classList.remove('top'); // Remove the 'top' class when closing the dropdown
         }
     });
 });
