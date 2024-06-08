e086bc299b56c7cacb5c6067a1c03882b96adef2
ifilmahd <65521650+ifilmahd@users.noreply.github.com>
2024-06-08 13:32:38 +0200

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -7,20 +7,18 @@ document.addEventListener("DOMContentLoaded", function() {
     dropdownToggle.addEventListener('click', function(event) {
         event.preventDefault();
         dropdownMenu.classList.toggle('show');
-
-        // Adjust the z-index dynamically
+    });
+// Adjust the z-index dynamically
         if (dropdownMenu.classList.contains('show')) {
             dropdownMenu.style.zIndex = "9999";
         } else {
             dropdownMenu.style.zIndex = "auto";
         }
     });
-
     // Close dropdown when clicking outside
     document.addEventListener('click', function(event) {
         if (!event.target.closest('.dropdown')) {
             dropdownMenu.classList.remove('show');
-            dropdownMenu.style.zIndex = "auto"; // Reset z-index when closing the dropdown
         }
     });
 
