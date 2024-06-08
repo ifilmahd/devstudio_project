3dd54c4389714a4b3c19e7e6d7b6e5b1dc843710
ifilmahd <65521650+ifilmahd@users.noreply.github.com>
2024-06-08 22:22:03 +0200

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -17,8 +17,10 @@ document.addEventListener("DOMContentLoaded", function() {
     if (savedLang) {
         const savedLangBase = getBaseLanguagePath(savedLang);
         const currentLangBase = getBaseLanguagePath(window.location.pathname);
-        if (!window.location.pathname.startsWith(savedLangBase)) {
-            window.location.href = savedLang;
+        if (currentLangBase === '/' && savedLangBase === '/it/') {
+            window.location.href = savedLang; // Redirect to the Italian language page
+        } else if (!window.location.pathname.startsWith(savedLangBase)) {
+            window.location.href = savedLang; // Redirect to the saved language page if not already there
         }
     }
 
@@ -54,9 +56,7 @@ document.addEventListener("DOMContentLoaded", function() {
         localStorage.setItem('preferredLanguage', lang);
         const selectedLangBase = getBaseLanguagePath(lang);
         const currentLangBase = getBaseLanguagePath(window.location.pathname);
-        if (currentLangBase === '/' && selectedLangBase === '/it/') {
-            window.location.href = lang; // Redirect to the selected language page
-        } else if (!window.location.pathname.startsWith(selectedLangBase)) {
+        if (!window.location.pathname.startsWith(selectedLangBase)) {
             window.location.href = lang; // Redirect to the selected language page if not already there
         }
     }
