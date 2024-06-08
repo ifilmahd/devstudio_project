d3c414e736d062a8bd2b9baef9c77172fe181163
ifilmahd <65521650+ifilmahd@users.noreply.github.com>
2024-06-08 23:45:03 +0200

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -41,12 +41,13 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 function setLanguage(lang) {
-        const currentLang = window.location.pathname.split('/')[1]; // Extract current language
-        const newLang = lang.split('/')[3]; // Extract selected language
-        if (currentLang !== newLang) {
-            const newUrl = new URL(lang, window.location.origin).href;
-            localStorage.setItem('preferredLanguage', newLang);
-            window.location.href = newUrl; // Redirect to the selected language page
-        }
+    const currentLang = window.location.pathname.split('/')[1] || ''; // Extract current language or set to empty string if not present
+    const newLang = lang.split('/')[3] || ''; // Extract selected language or set to empty string if not present
+    if (currentLang !== newLang) {
+        const newPath = lang.startsWith('/') ? lang : '/' + lang; // Ensure the path starts with a slash
+        const newUrl = new URL(newPath, window.location.origin).href;
+        localStorage.setItem('preferredLanguage', newLang);
+        window.location.href = newUrl; // Redirect to the selected language page
     }
-});
+}
+
