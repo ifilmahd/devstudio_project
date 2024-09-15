94694b8b5db985fa3f8c970e7844d10d3aa6d9da
ifilmahd <65521650+ifilmahd@users.noreply.github.com>
2024-09-15 03:10:15 +0200

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,24 +1,16 @@
 <script>
 document.addEventListener('DOMContentLoaded', function () {
     // Function to handle language switch
-    function switchLanguage(language) {
-        // Define the base URL for each language
-        let baseUrlEnglish = 'https://devstudioal.com/';
-        let baseUrlGerman = 'https://devstudioal.com/de/';
-
-        // Determine which URL to redirect to based on selected language
-        let newUrl = language === 'de' ? baseUrlGerman : baseUrlEnglish;
-
-        // Redirect to the selected language URL
-        window.location.href = newUrl;
+    function switchLanguage(url) {
+        window.location.href = url;
     }
 
     // Add event listeners to the dropdown items
     document.querySelectorAll('.dropdown-item').forEach(item => {
         item.addEventListener('click', function (event) {
             event.preventDefault();  // Prevent the default action of the anchor tag
-            let language = this.href.includes('/de/') ? 'de' : 'en';
-            switchLanguage(language);
+            let url = this.href;
+            switchLanguage(url);
         });
     });
 });
