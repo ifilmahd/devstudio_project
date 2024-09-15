75daa6f54df37212149eb27a1704aba3679d151a
ifilmahd <65521650+ifilmahd@users.noreply.github.com>
2024-09-15 03:16:30 +0200

    Update dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,16 +1,11 @@
 <script>
 document.addEventListener('DOMContentLoaded', function () {
-    // Function to handle language switch
-    function switchLanguage(url) {
-        window.location.href = url;
-    }
-
-    // Add event listeners to the dropdown items
+    // Add event listeners to dropdown items
     document.querySelectorAll('.dropdown-item').forEach(item => {
         item.addEventListener('click', function (event) {
-            event.preventDefault();  // Prevent the default action of the anchor tag
-            let url = this.href;
-            switchLanguage(url);
+            event.preventDefault();  // Prevent default link behavior
+            let url = this.getAttribute('href'); // Get the URL from the href attribute
+            window.location.href = url;  // Redirect to the selected URL
         });
     });
 });
